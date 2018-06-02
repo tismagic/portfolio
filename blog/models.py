8c9f40537d6511af915a9922bcdd4441bc27b771
Unknown <595521697@qq.com>
2018-06-02 15:57:02 +0800

    添加了blog App，美化了blog页面

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Blog(models.Model):
+	title = models.CharField(default='文章标题', max_length=50)
+	date = models.DateField()
+	image = models.ImageField(default='default.png', upload_to='images/')
+	text = models.TextField(default='正文')
+	
+	def __str__(self):
+		return self.title
+
+	def short_text(self):
+		return self.text[:60] + '...'
+    
